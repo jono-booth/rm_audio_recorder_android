f5d22d64a0cae77dac05167402520c8014789610
Jono <jono@platform45.com>
2015-05-20 12:01:44 +0200

    :star2: added circle buttons and recording pulser with gradle

--- app/main_activity.rb
+++ app/main_activity.rb
@@ -5,14 +5,35 @@ class MainActivity < Android::App::Activity
     super
     setContentView(R::Layout::Main)
 
+    @rippleBackground = findViewById(R::Id::Content)
+
     @record_button = findViewById(R::Id::Record_button)
-    @record_button.onClickListener = self
+    @stop_button = findViewById(R::Id::Stop_button)
+
+    @record_button.onClickListener = @stop_button.onClickListener = self
     @recording = false
   end
 
   def onClick(view)
     @recording = !@recording
-    text = @recording ? R::String::Record_button_on : R::String::Record_button_off
-    @record_button.text = getString(text)
+    if @recording
+      start_recording
+    else
+      stop_recording
+    end
+  end
+
+  def start_recording
+    @record_button.setVisibility(4)
+    @stop_button.setVisibility(0)
+
+    @rippleBackground.startRippleAnimation
+  end
+
+  def stop_recording
+    @stop_button.setVisibility(4)
+    @record_button.setVisibility(0)
+
+    @rippleBackground.stopRippleAnimation
   end
 end
